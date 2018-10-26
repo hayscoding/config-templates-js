d26a19c1c97179d60b4d5252ada1352af997d10f
HaysS <stanfordhays@gmail.com>
2018-10-26 18:27:06 -0500

    added config for projects using firebase (forgot to git add before)

---
+++ firebase-config.js
@@ -0,0 +1,43 @@
+// Config template for projects using Firebase
+// Author: E. Hays Stanford
+
+var config = {
+	development: {
+	    //url to be used in link generation
+	    url: '',
+	    //firebase connection settings
+	    database: {
+			firebaseApiKey: "",
+			firebaseAuthDomain: "",
+			databaseURL: "",
+ 		},
+	    //server details
+	    server: {
+	        baseUrl: ''
+	    },
+	    //facebook api details
+	    facebook: {
+	    	appId: ''
+	    },
+	},
+	production: {
+	    //url to be used in link generation
+	    url: '',
+	    //firebase connection settings
+	    database: {
+			firebaseApiKey: "",
+			firebaseAuthDomain: "",
+			databaseURL: ""
+	 	}, 
+	    //server details
+	    server: {
+	        baseUrl: ''
+	    },
+	    //facebook api details
+	    facebook: {
+	    	appId: ''
+	    },
+	}
+};
+
+module.exports = config;
